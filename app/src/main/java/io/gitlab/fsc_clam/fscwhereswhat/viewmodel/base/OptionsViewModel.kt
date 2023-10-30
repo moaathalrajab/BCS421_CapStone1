3b2e8ba4ede2daf796e58d00c5862efb9d21f73b
harvey <tsenh@farmingdale.edu>
2023-10-30 18:34:09 -0400

    Commit work on Onboarding ViewModels
    Work from group meeting 2023-10-30

---
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/viewmodel/base/OptionsViewModel.kt
@@ -0,0 +1,32 @@
+/*
+ *  Copyright (c) 2023 TEAM CLAM
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.gitlab.fsc_clam.fscwhereswhat.viewmodel.base
+
+import android.graphics.Color
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.StateFlow
+
+abstract class OptionsViewModel : ViewModel() {
+	abstract val buildingColor: StateFlow<Color>
+	abstract val eventColor: StateFlow<Color>
+
+	abstract fun setBuildingColor(color: Color)
+
+	abstract fun setEventColor(color: Color)
+
+}

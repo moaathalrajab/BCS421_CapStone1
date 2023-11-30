5072bed8c64c9b060e8446a694cd5a3caa3d70b8
harvey <tsenh@farmingdale.edu>
2023-11-29 20:26:25 -0500

    Add OSM dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -127,4 +127,9 @@ dependencies {
 	implementation("org.osmdroid:osmdroid-android:6.1.17")
 	implementation("tech.utsmankece:osm-androd-compose:+")
 
+	//OSM
+	implementation("org.osmdroid:osmdroid-android:6.1.17")
+	implementation("tech.utsmankece:osm-androd-compose:+")
+	implementation("com.google.android.gms:play-services-location:21.0.1")
+
 }

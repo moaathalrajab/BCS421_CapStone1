fe591e9a611b2402724e194e9d85df0a9986d805
harvey <tsenh@farmingdale.edu>
2023-11-29 20:28:02 -0500

    Create Fake Repos for ImplMapViewModel

---
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/repo/impl/FakeLocationRepo.kt
@@ -0,0 +1,36 @@
+/*
+ *  Copyright (c) 2023 TEAM CLAM
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.gitlab.fsc_clam.fscwhereswhat.repo.impl
+
+import io.gitlab.fsc_clam.fscwhereswhat.repo.base.LocationRepo
+import kotlinx.coroutines.flow.StateFlow
+
+class FakeLocationRepo: LocationRepo {
+	override val longitude: StateFlow<Float>
+		get() = TODO("Not yet implemented")
+	override val latitude: StateFlow<Float>
+		get() = TODO("Not yet implemented")
+
+	override fun setLongitude(longitude: Float) {
+		TODO("Not yet implemented")
+	}
+
+	override fun setLatitude(latitude: Float) {
+		TODO("Not yet implemented")
+	}
+}

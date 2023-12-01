92589eff1b528330a7bfb8be3b824f6d8f5502c8
harvey <tsenh@farmingdale.edu>
2023-12-01 18:21:04 -0500

    Create placeholder for EntityDetail to show when user clicks pinpoint

---
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/map/EntityDetail.kt
@@ -0,0 +1,80 @@
+/*
+ *  Copyright (c) 2023 TEAM CLAM
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.gitlab.fsc_clam.fscwhereswhat.ui.map
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+
+@Composable
+fun EntityDetail(){
+	Card(
+		modifier = Modifier
+			.padding(16.dp)
+			.fillMaxSize()
+	){
+		Column(
+			modifier = Modifier.fillMaxHeight(.7f),
+			verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterVertically),
+			horizontalAlignment = Alignment.CenterHorizontally
+		) {
+			Text(text = "Title")
+			Row(
+				modifier = Modifier.fillMaxWidth(),
+				horizontalArrangement = Arrangement.SpaceAround
+			) {
+				Text(text = "Room Name")
+				Text(text = "Room Number")
+			}
+			Row {
+				Text(text = "Date and Time")
+			}
+			AsyncImage(
+				model = "",
+				contentDescription = "",
+				modifier = Modifier.weight(1f)
+			)
+		}
+	}
+}
+
+@Preview
+@Composable
+fun PreviewEntityDetail(){
+	Box(
+		modifier = Modifier
+			.padding(8.dp)
+			.fillMaxHeight(.7f)
+	){
+		EntityDetail()
+	}
+}

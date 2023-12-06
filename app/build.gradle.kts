b402dc94dca11c9ee8c13a087d3062e257f24bf3
harvey <tsenh@farmingdale.edu>
2023-12-06 12:12:12 -0500

    Remove google play services location

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -115,6 +115,5 @@ dependencies {
 	//OSM
 	implementation("org.osmdroid:osmdroid-android:6.1.17")
 	implementation("tech.utsmankece:osm-androd-compose:+")
-	implementation("com.google.android.gms:play-services-location:21.0.1")
 
 }

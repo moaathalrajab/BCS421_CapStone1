b8cc1c94953c85120f97ff1a7bdb0c68108d1d0f
Rahim Akhter <akhter.s.rahim@gmail.com>
2023-12-06 10:28:32 -0500

    Implement MainView with NavHost

--- app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/MainActivity.kt
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/MainActivity.kt
@@ -20,14 +20,7 @@ package io.gitlab.fsc_clam.fscwhereswhat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import io.gitlab.fsc_clam.fscwhereswhat.ui.theme.FSCWheresWhatTheme
+import io.gitlab.fsc_clam.fscwhereswhat.ui.main.MainView
 
 class MainActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,45 +28,5 @@ class MainActivity : ComponentActivity() {
 		setContent {
 			MainView()
 		}
-	}
-}
-
-@Composable
-fun MainView() {
-	FSCWheresWhatTheme {
-		MainContent()
-	}
-}
-
-@Preview
-@Composable
-fun PreviewMainContent() {
-	MainContent()
-}
-
-@Composable
-fun MainContent() {
-	// A surface container using the 'background' color from the theme
-	Surface(
-		modifier = Modifier.fillMaxSize(),
-		color = MaterialTheme.colorScheme.background
-	) {
-		Greeting("Android")
-	}
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-	Text(
-		text = "Hello $name!",
-		modifier = modifier
-	)
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PreviewGreeting() {
-	FSCWheresWhatTheme {
-		Greeting("Android")
 	}
 }

b6ffe4949b5737f3d0329222832b4e469ea9580e
liivsan <sanfol@farmingdale.edu>
2023-12-11 16:03:19 -0500

    SearchView design and dummy data

--- app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/search/SearchView.kt
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/search/SearchView.kt
@@ -17,14 +17,23 @@
 
 package io.gitlab.fsc_clam.fscwhereswhat.ui.search
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
@@ -40,9 +49,29 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.gitlab.fsc_clam.fscwhereswhat.R
+
+data class SearchItem(val name: String, val description: String) // example data class for show
+@Preview
+@Composable
+fun SearchViewPreview() {
+	val searchItem = SearchItem(name = "Item 1", description = "Description for Item 1")
+
+	Column(
+			modifier = Modifier
+			.fillMaxSize()
+			.padding(16.dp)
+			.background(color = Color.DarkGray)
+	) {
+		SearchView()
+		SearchViewList()
+		SearchViewItem(searchItem)
+	}
+}
 
 @Composable
 fun SearchView() {
@@ -53,7 +82,7 @@ fun SearchView() {
 		Column(
 			modifier = Modifier
 				.padding(12.dp)
-				.fillMaxSize(),
+				.fillMaxWidth(),
 			verticalArrangement = Arrangement.spacedBy(8.dp),
 			horizontalAlignment = Alignment.CenterHorizontally
 		) {
@@ -80,6 +109,56 @@ fun SearchView() {
 					)
 
 				}
+			}
+		}
+	}
+}
+
+@Composable
+fun SearchViewList() {
+	val items = List(10) { index -> // EXAMPLES
+		SearchItem(name = "Item $index", description = "Description for Item $index")
+	}
+
+	LazyColumn(contentPadding = PaddingValues(16.dp)) {
+		items.forEach { item ->
+			item {
+				SearchViewItem(item)
+			}
+		}
+	}
+}
+
+@Composable
+fun SearchViewItem(item: SearchItem) {
+	val scrollState = rememberScrollState()
+	Card(
+		modifier = Modifier
+			.padding(10.dp)
+			.wrapContentSize()
+	) {
+		Row(
+			modifier = Modifier
+				.fillMaxWidth()
+				.height(100.dp),
+			verticalAlignment = Alignment.Top,
+			horizontalArrangement = Arrangement.spacedBy(15.dp)
+		) {
+			Image(
+				painter = painterResource(id = R.drawable.wheres_what_logo),
+				contentDescription = null,
+				modifier = Modifier.size(115.dp)
+			)
+			Column(
+				modifier = Modifier.padding(12.dp)
+					.verticalScroll(scrollState),
+				verticalArrangement = Arrangement.spacedBy(20.dp)
+			)
+			{
+				Text(
+					text = item.name
+				)
+				Text(text = item.description)
 			}
 		}
 	}

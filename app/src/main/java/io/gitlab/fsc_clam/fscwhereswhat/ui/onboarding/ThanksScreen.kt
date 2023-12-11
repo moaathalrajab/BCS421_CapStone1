20cf8e4e77379735c2b95c189f9f3b38f9581bda
harvey <tsenh@farmingdale.edu>
2023-12-11 07:52:12 -0500

    Adapt ThanksScreen for landscape and rotation

--- app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/onboarding/ThanksScreen.kt
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/onboarding/ThanksScreen.kt
@@ -21,7 +21,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -57,7 +59,8 @@ import io.gitlab.fsc_clam.fscwhereswhat.ui.theme.titleFont
 fun ThanksScreen(
 	onFinish: () -> Unit
 ) {
-	Box(Modifier.fillMaxSize()) {
+	BoxWithConstraints(Modifier.fillMaxSize()) {
+		if(maxHeight > 480.dp)
 		//Creates white box for content
 		Box(
 			modifier = Modifier
@@ -109,6 +112,58 @@ fun ThanksScreen(
 						color = Color.Black
 					)
 
+				}
+			}
+		}
+		else{
+			Row(
+				modifier = Modifier
+					.fillMaxSize(.9f)
+					.background(Color.White)
+					.align(Alignment.Center),
+				horizontalArrangement = Arrangement.SpaceBetween
+			) {
+				//Developer logo
+				Image(
+					painter = painterResource(id = R.drawable.team_clam_logo),
+					contentDescription = stringResource(id = R.string.thanks_developers_logo),
+					contentScale = ContentScale.Crop,
+					modifier = Modifier
+						.padding(top = 5.dp, bottom = 15.dp)
+						.size(280.dp)
+						.clip(RoundedCornerShape(25))
+				)
+				Column(
+					modifier = Modifier.fillMaxSize(),
+					horizontalAlignment = Alignment.CenterHorizontally,
+					verticalArrangement = Arrangement.spacedBy(8.dp)
+				) {
+					Text(
+						text = stringResource(id = R.string.thanks_heading),
+						fontFamily = titleFont,
+						fontWeight = FontWeight.Bold,
+						fontStyle = FontStyle.Normal,
+						fontSize = 46.sp,
+					)
+					Text(
+						text = stringResource(id = R.string.thanks_body),
+						fontFamily = titleFont,
+						fontWeight = FontWeight.Bold,
+						fontStyle = FontStyle.Normal,
+						fontSize = 38.sp,
+						modifier = Modifier.padding(top = 15.dp, bottom = 25.dp)
+					)
+					//Button to navigate to MapView
+					Button(
+						onClick = onFinish,
+						colors = ButtonDefaults.buttonColors(primaryColor),
+					) {
+						Text(
+							text = stringResource(id = R.string.thanks_continue_button),
+							color = Color.Black
+						)
+
+					}
 				}
 			}
 		}

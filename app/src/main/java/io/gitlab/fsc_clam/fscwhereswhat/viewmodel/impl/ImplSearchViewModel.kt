4ea9a01ee0d5f685145bde9f47743656394f8247
liivsan <sanfol@farmingdale.edu>
2023-12-11 16:16:46 -0500

    Implement ImplSearchViewModel.
    It manages the user input query, the active filter for EntityType, and the list of entities for search results.
    
    The updateEntities method is used for fetching and updating the list of entities based on the current query and active filter.

---
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/viewmodel/impl/ImplSearchViewModel.kt
@@ -0,0 +1,86 @@
+/*
+ *  Copyright (c) 2023 TEAM CLAM
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package io.gitlab.fsc_clam.fscwhereswhat.viewmodel.impl
+
+import android.app.Application
+import io.gitlab.fsc_clam.fscwhereswhat.model.local.EntityItem
+import io.gitlab.fsc_clam.fscwhereswhat.model.local.EntityType
+import io.gitlab.fsc_clam.fscwhereswhat.viewmodel.base.SearchViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * Implementation of the SearchViewModel.
+ *
+ * It manages the user input query, active filter, and the list of entities for search results.
+ *
+ * @param application The application context.
+ */
+class ImplSearchViewModel(application: Application) : SearchViewModel(application) {
+
+	/**
+	 * Holds the user input from the search bar.
+	 */
+	private val _query = MutableStateFlow("")
+	override val query: StateFlow<String>
+		get() = _query
+
+	/**
+	 * Holds the active filter for EntityType to show results for Building, Event, Node, or All.
+	 */
+	private val _activeFilter = MutableStateFlow<EntityType?>(null)
+	override val activeFilter: StateFlow<EntityType?>
+		get() = _activeFilter
+
+	/**
+	 * Holds the list of entities to populate search results.
+	 */
+	private val _entities = MutableStateFlow<List<EntityItem>>(emptyList())
+	override val entities: StateFlow<List<EntityItem>>
+		get() = _entities
+
+	/**
+	 * Sets the user input query.
+	 *
+	 * @param query The string input from the user.
+	 */
+	override fun setQuery(query: String) {
+		_query.value = query
+	}
+
+	/**
+	 * Changes the active filter for EntityType.
+	 *
+	 * @param filter The entity type being filtered.
+	 */
+	override fun setActiveFilter(filter: EntityType?) {
+		_activeFilter.value = filter
+		// update the entities based on the new filter
+		updateEntities()
+	}
+
+	/**
+	 * Fetches and updates the list of entities based on the current query and active filter.
+	 * Uses ______ repository
+	 */
+	private fun updateEntities() {
+		// fetch and update the list of entities based on the current query and active filter.
+		// Use repo here
+		// _entities.value =
+	}
+}

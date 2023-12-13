4eea19e8b5cef560b4cf7f57e445815c3b90da17
harvey <tsenh@farmingdale.edu>
2023-12-13 18:30:28 -0500

    Clean up imports for MapContent

--- app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/map/MapContent.kt
+++ app/src/main/java/io/gitlab/fsc_clam/fscwhereswhat/ui/map/MapContent.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -61,7 +60,6 @@ import org.osmdroid.util.GeoPoint
  * @param navigateToMore navigates to MoreView
  * @param login handles google login
  */
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MapContent(
 	snackbarState: SnackbarHostState,
